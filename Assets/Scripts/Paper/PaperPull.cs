05ceddeffc39e3745142943d1df3f7bd312074b6
Shota HAMADA <maku3ts@gmail.com>
2018-09-26 23:12:05 +0900

    Show length of pulled paper

---
+++ Assets/Scripts/Paper/PaperPull.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PaperPull : MonoBehaviour
+{
+    private const float inch2cm = 2.54F;
+    private float pulledLength;
+    [SerializeField]
+    private string pulledLengthFormat;
+    private string pulledLengthString => pulledLength.ToString(pulledLengthFormat) + "m";
+
+    private bool isPulled;
+    private Vector2 pullStart;
+    private float pullStartTime;
+    private float pullSpeed;
+
+    [SerializeField]
+    private float pullSpeedMultiplier;
+    [SerializeField]
+    private float pullSpeedFriction;
+    private float pullSpeedDamping => 1.0F - pullSpeedFriction;
+
+    [SerializeField]
+    private float pullProgressTorelance;
+    private bool isStopped;
+
+    [SerializeField]
+    private Text pulledLengthText;
+
+    private void Enable()
+    {
+        isPulled = false;
+        isStopped = false;
+    }
+
+    private void Start()
+    {
+        // For demonstration
+        pullSpeed = 10F;
+    }
+
+    private void Update()
+    {
+        Pull();
+        UpdatePulledLengthText();
+    }
+
+    private void Pull()
+    {
+        if (isStopped) { return; }
+
+        GetPullInput();
+
+        var pullProgress = pullSpeed * pullSpeedMultiplier * Time.deltaTime;
+        if (pullProgress < pullProgressTorelance)
+        {
+            isStopped = true;
+            return;
+        }
+        pulledLength += pullProgress;
+        pullSpeed *= pullSpeedDamping;
+    }
+
+    private void GetPullInput()
+    {
+        if (isPulled) { return; }
+
+        if (Input.touchCount == 0) { return; }
+
+        var touch = Input.GetTouch(0);
+        if (touch.phase == TouchPhase.Began)
+        {
+            pullStart = touch.position;
+            pullStartTime = Time.time;
+            return;
+        }
+
+        if (touch.phase != TouchPhase.Ended) { return; }
+        isPulled = true;
+
+        var pullDistance = (touch.position - pullStart).magnitude;
+        var pullDistanceInCm = pullDistance / Screen.dpi * inch2cm;
+        var pullDuration = Time.time - pullStartTime;
+        pullSpeed = pullDistanceInCm / pullDuration;
+    }
+
+    private void UpdatePulledLengthText()
+    {
+        pulledLengthText.text = pulledLengthString;
+    }
+}
